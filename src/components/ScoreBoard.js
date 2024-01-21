26c860eb5c7c34978824377e5eb0e2bad753c652
Bilal El-aney <bilalaney084@gmail.com>
2024-01-21 22:02:32 +0300

    Fixing the timer bug

---
+++ src/components/ScoreBoard.js
@@ -0,0 +1,7 @@
+/** @format */
+
+import { useState } from "react";
+
+function ScoreBoard(props) {
+  const [winList, setWindList] = useState([]);
+}
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -4,17 +4,25 @@ import { useState, useEffect, memo, useRef } from "react";
 
 function Timer(props) {
   const [count, setCount] = useState(0);
-  const ref = useRef(null);
+
   useEffect(() => {
-    if (props.restart) setCount(0);
-    else ref.current = setInterval(() => setCount((pre) => pre + 1), 1000);
-    return clearInterval(ref.current);
-  }, [props.restart]);
+    const id = setInterval(() => setCount((pre) => pre + 1), 1000);
+    return () => clearInterval(id);
+  }, []);
 
-  let seconds = `0${Math.floor(count % 60)}`.slice(-2);
-  let minutes = `0${Math.floor((count / 60) % 60)}`.slice(-2);
+  //note that the following are derived states
+  const seconds = `0${Math.floor(count % 60)}`.slice(-2);
+  const minutes = `0${Math.floor((count / 60) % 60)}`.slice(-2);
 
-  return <div className="theTimer">{minutes + ":" + seconds}</div>;
+  const styles = {
+    margin: "25px",
+  };
+
+  return (
+    <div style={styles} className="theTimer">
+      {minutes + ":" + seconds}
+    </div>
+  );
 }
 
 export default memo(Timer);
